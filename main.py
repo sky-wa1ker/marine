7961eae6a64e7ab8411a58194c20db01b1355445
Shravan Kumar <shravan542316@gmail.com>
2023-09-16 18:44:36 +0530

    Refactor code and add chatGPT command

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ from http import client
 import os
 import discord
 from discord.ext import commands, tasks
-import re
-import json
 from datetime import datetime
 from translate import Translator
-import time
+import openai
+
+
+
 
 
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix="m!", intents = intents)
 token = (os.environ["DISCORD_TOKEN"])
-
+openai.api_key = (os.environ["OPENAI_KEY"])
 
 
 
@@ -21,8 +22,6 @@ token = (os.environ["DISCORD_TOKEN"])
 async def on_ready():
     game = discord.Game("with the crew.")
     await client.change_presence(status=discord.Status.online, activity=game)
-    if not up.is_running():
-        up.start()
     print('Online as {0.user}'.format(client))
 
 @client.event
@@ -33,14 +32,13 @@ async def on_command_error(ctx, error):
 
 
 
-
 @client.command()
 @commands.cooldown(1, 10, commands.BucketType.user)
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency*1000)}ms')
 
 
-@client.command()
+@client.slash_command()
 async def translate(ctx, from_lang, to_lang):
     translator= Translator(to_lang=to_lang, from_lang=from_lang, secret_access_key=None, base_url='https://translate.astian.org/')
     message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
@@ -48,11 +46,22 @@ async def translate(ctx, from_lang, to_lang):
     await ctx.send(str(translation))
 
 
+@client.slash_command(description="chatGPT command.")
+async def chat(ctx, prompt:str, init_command:str="You are a very smart assistant."):
+    role = discord.utils.get(ctx.guild.roles, name="Captain")
+    if role in ctx.author.roles:
+        await ctx.defer()
+        response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": f"{init_command}"},
+            {"role": "user", "content": f"{prompt}"}
+            ]
+        )
+        await ctx.respond(response['choices'][0]['message']['content'])
+    else:
+        await ctx.send("this command is only for arrgh captains.")
 
-@tasks.loop(minutes=1)
-async def up():
-    channel = client.get_channel(520567638779232256)
-    await channel.send("<@343397899369054219> hi I am up.")
 
 
 
